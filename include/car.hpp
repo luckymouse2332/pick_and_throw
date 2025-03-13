83cd5a370b245e85f5a58d0dd7c3cc2cd951fb0b
luckymouse2332 <mousexiaoqi111@163.com>
2025-03-13 21:24:14 +0800

    3.13 写好沿直线寻路

---
+++ include/car.hpp
@@ -0,0 +1,56 @@
+#ifndef CAR_HPP
+#define CAR_HPP
+
+class CarController
+{
+public:
+  CarController(int lf_pin, int rf_pin, int ls_pin, int rs_pin)
+      : left_forward_pin(lf_pin), right_forward_pin(rf_pin), left_speed_pin(ls_pin), right_speed_pin(rs_pin) {}
+
+  /**
+   * 初始化端口
+   */
+  void init();
+
+  /**
+   * 启动
+   */
+  void start();
+
+  /**
+   * 左转
+   */
+  void turn_left();
+
+  /**
+   * 右转
+   */
+  void turn_right();
+
+  /**
+   * 停车
+   */
+  void stop();
+
+private:
+  /**
+   * 左轮电机转向控制端口
+   */
+  int left_forward_pin;
+
+  /**
+   * 右轮电机转向控制端口
+   */
+  int right_forward_pin;
+
+  /**
+   * 左轮转速控制端口
+   */
+  int left_speed_pin;
+
+  /**
+   * 右轮转速控制端口
+   */
+  int right_speed_pin;
+};
+#endif
---
+++ src/car.cpp
@@ -0,0 +1,40 @@
+#include "car.hpp"
+#include <Arduino.h>
+
+void CarController::init()
+{
+  pinMode(left_forward_pin, OUTPUT);
+  pinMode(right_forward_pin, OUTPUT);
+  pinMode(left_speed_pin, OUTPUT);
+  pinMode(right_speed_pin, OUTPUT);
+}
+
+void CarController::start()
+{
+  // 初始化转向
+  digitalWrite(left_forward_pin, LOW);
+  digitalWrite(right_forward_pin, LOW);
+
+  // 初始化转速
+  analogWrite(left_speed_pin, 180);
+  analogWrite(right_speed_pin, 255);
+}
+
+void CarController::turn_left()
+{
+  start(); // 先重置参数再停转
+  analogWrite(left_speed_pin, 0);
+}
+
+void CarController::turn_right()
+{
+  start();
+  analogWrite(right_speed_pin, 0);
+}
+
+void CarController::stop()
+{
+  // 初始化转速
+  analogWrite(left_speed_pin, 0);
+  analogWrite(right_speed_pin, 0);
+}
